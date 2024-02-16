260ccb84057f936a6ff8d468c4d34a138c0d8784
root <root@localhost.localdomain>
2024-02-16 14:17:26 +0530

    first files

---
+++ for1.go
@@ -0,0 +1,13 @@
+// for loop
+
+package main
+import "fmt"
+
+func main(){
+	a:=1
+	for a <=10 {
+		fmt.Println(a*a)
+		
+		a+=1
+	}
+}
---
+++ govar.go
@@ -0,0 +1,18 @@
+// var
+package main
+
+import "fmt"
+
+var a =28
+var a1 string = "ali"
+var a2 =2.0
+var a3 = false
+ 
+
+func main(){
+
+  fmt.Println(a,a1,a2,a3)
+  fmt.Println(a1[0])
+
+
+}
